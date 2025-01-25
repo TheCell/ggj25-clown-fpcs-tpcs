b729d57164fe3aa527f2e76d090010eaace72152
Simon Hischier <simon_hischier@hotmail.com>
2025-01-25 19:59:09 +0100

    WIP interactionhistory

---
+++ Assets/Scripts/NPC/InteractionHistory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InteractionHistory : MonoBehaviour
+{
+    //private 
+
+    private void AddHasBeenInteractedWith(Interaction interaction)
+    {
+        // Add the interaction to the list of interactions
+    }
+}
